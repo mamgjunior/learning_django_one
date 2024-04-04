fa61fbbb9a99e89e5ec6d630de0e0662cf9d1c0c
Marcos Antonio Maia Gomes Junior <marcos.gomes@underprotection.com.br>
2024-04-03 21:09:46 -0300

    Templates e boas práticas

--- galeria/views.py
+++ galeria/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request=request, template_name='galeria/index.html')
+
+def imagem(request):
+    return render(request=request, template_name='galeria/imagem.html')
