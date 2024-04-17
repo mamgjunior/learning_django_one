cb0553f7acc05ed495287228948064c4840240a1
Marcos Antonio Maia Gomes Junior <marcos.gomes@underprotection.com.br>
2024-04-16 21:17:28 -0300

    Avançando no admin

--- galeria/views.py
+++ galeria/views.py
@@ -2,12 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
 def index(request):
-    fotografia = Fotografia.objects.all()
-    return render(
-        request, 
-        'galeria/index.html', 
-        {"cards": fotografia}
-    )
+    fotografia = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+    return render(request, 'galeria/index.html', {"cards": fotografia})
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
