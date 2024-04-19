4523edf7c107629961a8b0cfacaaef142ea1ae38
Marcos Antonio Maia Gomes Junior <marcos.gomes@underprotection.com.br>
2024-04-19 17:28:41 -0300

    Ajustando imagens e filtros

--- setup/urls.py
+++ setup/urls.py
@@ -8,4 +8,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('galeria.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
