3f7f79b569653c48b9e9e5a72b175faf5fa50ed2
mamgjunior <mamgjunior@hotmail.com>
2024-04-26 11:07:33 -0300

    formulário de cadastro

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -23,3 +23,50 @@ class LoginForms(forms.Form):
             }
         )
     )
+
+
+class CadastroForms(forms.Form):
+    nome_login=forms.CharField(
+        label='Nome de Cadastro', 
+        required=True, 
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex.: João Silva',
+            }
+        )
+    )
+    email=forms.EmailField(
+        label='Digite seu email',
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex.: joaosilva@xpto.com',
+            }
+        )
+    )
+    senha_cadastro=forms.CharField(
+        label='Senha', 
+        required=True, 
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha',
+            }
+        ),
+    )
+    senha_confirmacao=forms.CharField(
+        label='Confirme sua senha', 
+        required=True, 
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha novamente',
+            }
+        ),
+    )
